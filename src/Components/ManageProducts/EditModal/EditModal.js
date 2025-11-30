b3f92910fa4f50db2009a98848c3a3c2753479b4
Mohsen <mohsenwz20201@gmail.com>
2025-11-30 07:06:16 -0800

    Create EditModal component.
    For ManageProducts Page.

---
+++ src/Components/ManageProducts/EditModal/EditModal.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import './EditModal.css'
+
+export default function EditModal({details, onclose}) {
+    const [detailsCopy, setDetailsCopy] = useState({})
+    useEffect(() => {
+        setDetailsCopy(details)
+    },[])
+  return (
+    <div className='edit-modal-main-div'>
+        <div className="edit-modal-input-div">
+            <p className='edit-modal-input-title'>نام محصول:</p>
+            <input
+             type="text"
+             className="edit-modal-input"
+             value={detailsCopy.name} onChange={e => setDetailsCopy({...detailsCopy, name: e.target.value})} />
+        </div>
+        <div className="edit-modal-input-div">
+            <p className='edit-modal-input-title'>قیمت:</p>
+            <input
+             type="text"
+             className="edit-modal-input"
+             value={detailsCopy.price} onChange={e => setDetailsCopy({...detailsCopy, price: e.target.value})} />
+        </div>
+        <div className="edit-modal-input-div">
+            <p className='edit-modal-input-title'>موجودی:</p>
+            <input
+             type="number"
+             className="edit-modal-input"
+             value={detailsCopy.inventory} onChange={e => setDetailsCopy({...detailsCopy, inventory: e.target.value})} />
+        </div>
+        <div className="edit-modal-btns-div">
+            <button className='edit-modal-btn'>ثبت</button>
+            <button className='edit-modal-btn' onClick={() => onclose()}>انصراف</button>
+        </div>
+    </div>
+  )
+}
--- src/Components/ManageProducts/ManageProducts.js
+++ src/Components/ManageProducts/ManageProducts.js
@@ -1,22 +1,39 @@
 import './ManageProducts.css'
-import Row from '../Row/Row'
+import { useState } from 'react'
 import data from '../../Data/data.json'
+import Row from '../Row/Row'
+import EditModal from './EditModal/EditModal'
 
 export default function ManageProducts() {
+  const [modalToggle, setModalToggle] = useState(false)
+  const [productDetails, setProductDetails] = useState({})
 
-    const faNumHandler = num => new Intl.NumberFormat('fa-IR').format(num)
-    
+  const faNumHandler = num => new Intl.NumberFormat('fa-IR').format(num)
+  const editHandler = product => {
+    setProductDetails(product)
+    setModalToggle(true)
+  }
   return (
-    <div className='dash-page-content-parent'>
-        <div className='container'>
-            <p className='page-titles'>مدیریت محصولات</p>
-            <div className="manage-pros-main-div">
-              <Row borderDetails='1px solid #3d4d55' column1='نام محصول' column2='قیمت' column3='موجودی' ><div style={{width: "7rem"}}></div></Row>
-              {
-              data.products.map(prod => <Row key={prod.id} column1={prod.name} column2={`${faNumHandler(prod.price)} تومان`} column3={`${faNumHandler(prod.inventory)} عدد`}><button className='manage-pros-del-btn'>حذف</button></Row>)
-              }
-            </div>
-        </div>
-    </div>
+    <>
+      {modalToggle && <EditModal details={productDetails} onclose={() => setModalToggle(false)} />}
+      <div className='dash-page-content-parent'>
+          <div className='container'>
+              <p className='page-titles'>مدیریت محصولات</p>
+              <div className="manage-pros-main-div">
+                <Row borderDetails='1px solid #3d4d55' column1='نام محصول' column2='قیمت' column3='موجودی' >
+                  <div style={{width: "6rem"}}></div>
+                  <div style={{width: "6rem"}}></div>
+                </Row>
+
+                {data.products.map(prod =>
+                  <Row key={prod.id} column1={prod.name} column2={`${faNumHandler(prod.price)} تومان`} column3={`${faNumHandler(prod.inventory)} عدد`}>
+                    <button className='manage-pros-btn' onClick={() => editHandler(prod)}>ویرایش</button>
+                    <button className='manage-pros-btn'>حذف</button>
+                  </Row>
+                )}
+              </div>
+          </div>
+      </div>
+    </>
   )
 }
